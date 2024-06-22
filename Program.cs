ec1a3245cdecac90bf336f2559ae0cc4186ba886
Iroel Perez <iroel.perez@gmail.com>
2024-06-22 10:39:32 -0400

    First checkin

---
+++ Program.cs
@@ -0,0 +1,148 @@
+// 1- Add package: Microsoft.AspNetCore.Authentication.JwtBearer
+
+// 2- 
+//builder.Services.AddAuthentication(x =>
+//{
+//    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+//    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+//    x.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+//}).AddJwtBearer(x =>
+//{
+//    var config = builder.Configuration;
+//    x.TokenValidationParameters = new()
+//    {
+//        ValidIssuer = config["JwtSettings:Issuer"],
+//        ValidAudience = config["JwtSettings:Audience"],
+//        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["JwtSettings:Key"]!)),
+//        ValidateIssuer = true,
+//        ValidateAudience = true,
+//        ValidateLifetime = true,
+//        ValidateIssuerSigningKey = true
+//    };
+//});
+
+// 3-
+//builder.Services.AddAuthorization();
+
+// 4-
+// After app.UseHttpsRedirection(); and before MapControllers Add:
+// app.UseAuthentication();
+// app.UseAuthorization();
+
+using FastEndpoints;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddAuthentication(x => 
+{ 
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(x => 
+{
+    var config = builder.Configuration;
+    x.TokenValidationParameters = new()
+    {
+        ValidIssuer = config["JwtSettings:Issuer"],
+        ValidAudience = config["JwtSettings:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["JwtSettings:Key"]!)),
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true
+    };
+});
+
+builder.Services.AddAuthorization();
+
+builder.Services.AddFastEndpoints();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+app.UseFastEndpoints();
+
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast")
+.RequireAuthorization()
+.WithOpenApi();
+
+app.MapGet("/token", (IConfiguration config) =>
+{
+    return TokenHelper.GenerateValidToken(config);
+})
+.WithName("token")
+.WithOpenApi();
+
+app.Run();
+
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
+
+
+class TokenHelper
+{
+    public static string GenerateValidToken(IConfiguration config)
+    {
+        var claims = new[]
+        {
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+            new Claim(JwtRegisteredClaimNames.Email, "noemail"),
+            new Claim("uid", Guid.NewGuid().ToString())
+        };
+
+        var symmetricSecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["JwtSettings:Key"]!));
+        var signingCredentials = new SigningCredentials(symmetricSecurityKey, SecurityAlgorithms.HmacSha256);
+
+        var jwtSecurityToken = new JwtSecurityToken
+        (
+            issuer: config["JwtSettings:Issuer"],
+            audience: config["JwtSettings:Audience"],
+            claims: claims,
+            expires: DateTime.Now.AddMinutes(120),
+            signingCredentials: signingCredentials
+        );
+
+        return new JwtSecurityTokenHandler().WriteToken(jwtSecurityToken);
+    }
+}
---
+++ ProtectedData.cs
@@ -0,0 +1,33 @@
+﻿using FastEndpoints;
+
+namespace JwtMinimalWebApi;
+
+public class ProtectedData : EndpointWithoutRequest
+{
+    public override void Configure()
+    {
+        Get("/data");
+        //AllowAnonymous();
+    }
+
+    public override async Task HandleAsync(CancellationToken ct)
+    {
+        await SendAsync("You got it!");
+    }
+}
+
+
+public class ProtectedData2 : EndpointWithoutRequest
+{
+    public override void Configure()
+    {
+        Get("/data2");
+        // todo figure out policies!!!
+        Claims("email", "noemail");
+    }
+
+    public override async Task HandleAsync(CancellationToken ct)
+    {
+        await SendAsync("You got it!");
+    }
+}
